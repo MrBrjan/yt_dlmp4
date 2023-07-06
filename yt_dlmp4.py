3e75748e3682852bdf6b69b9d56bac7480606c6e
MrBrjan <hoangtanchuong@gmail.com>
2023-07-06 18:24:23 +0700

    add url_id

--- yt_dlmp4.py
+++ yt_dlmp4.py
@@ -3,7 +3,7 @@ import yt_dlp
 
 # Lấy URL hoặc ID của playlist từ người dùng.
 url_or_playlist_id = input("Nhập URL hoặc ID của playlist YouTube: ")
-
+url_id= "" 
 # Tạo một từ điển với các tùy chọn cho quá trình tải xuống.
 options = {
     'format': 'bestvideo+bestaudio/best',
